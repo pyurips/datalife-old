b031966b5d74f502fe9d086e424a89882e20ea3e
Pablo Yuri <pabloyuridev@gmail.com>
2023-12-02 08:08:58 -0300

    hotfix: removed bytecode js

--- resources/auth/src/client/index.ts
+++ resources/auth/src/client/index.ts
@@ -1,10 +1,10 @@
-import * as alt from "alt-client";
+import * as alt from 'alt-client';
 
-(async function() {
-    let view: alt.WebView;
-view = new alt.WebView("http://assets/webviews/auth/index.html");
-await new Promise((resolve: (...args: any[]) => void) => {
-  view.once("load", resolve);
-});
-view.focus();
+(async function () {
+  let view: alt.WebView;
+  view = new alt.WebView('http://assets/webviews/auth/index.html');
+  await new Promise((resolve: (...args: any[]) => void) => {
+    view.once('load', resolve);
+  });
+  view.focus();
 })();
