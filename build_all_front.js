2b5a86ac78d95814ed1050f9cfde13051fe4e08a
Pablo Yuri <pabloyuridev@gmail.com>
2023-12-04 05:54:12 -0300

    Updated all files organization

---
+++ build_all_front.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const path = require('path');
+const { exec } = require('child_process');
+
+const srcPath = './src/webviews';
+
+function buildWebviews() {
+  fs.readdir(srcPath, (err, folders) => {
+    if (err) {
+      console.error('Error reading webviews folders:', err);
+      return;
+    }
+
+    folders.forEach(folder => {
+      const folderPath = path.join(srcPath, folder);
+
+      fs.stat(folderPath, (err, stats) => {
+        if (err) {
+          console.error(`Error getting stats for ${folder}:`, err);
+          return;
+        }
+
+        if (stats.isDirectory()) {
+          const buildCommand = 'npm run build';
+          const options = { cwd: folderPath };
+
+          exec(buildCommand, options, (error, stdout, stderr) => {
+            if (error) {
+              console.error(`Error building ${folder}:`, error);
+              return;
+            }
+
+            console.log(`Built ${folder} successfully:\n${stdout}`);
+          });
+        }
+      });
+    });
+  });
+}
+
+// Run the buildWebviews function
+buildWebviews();
---
+++ toml_handler.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const path = require('path');
+const util = require('util');
+
+const readdir = util.promisify(fs.readdir);
+const copyFile = util.promisify(fs.copyFile);
+
+async function copyResourceFiles(srcPath, destPath) {
+  try {
+    const srcFolders = await readdir(srcPath, { withFileTypes: true });
+
+    for (const folder of srcFolders) {
+      if (folder.isDirectory()) {
+        const srcFolderPath = path.join(srcPath, folder.name);
+        const destFolderPath = path.join(destPath, folder.name);
+
+        const srcFiles = await readdir(srcFolderPath);
+        const resourceFile = srcFiles.find(file => file === 'resource.toml');
+
+        if (resourceFile) {
+          await copyFile(path.join(srcFolderPath, resourceFile), path.join(destFolderPath, resourceFile));
+          console.log(`Copied ${resourceFile} from ${srcFolderPath} to ${destFolderPath}`);
+        }
+      }
+    }
+
+    console.log('Copy process completed.');
+  } catch (error) {
+    console.error('Error copying resource files:', error);
+  }
+}
+
+// Usage example:
+const srcPath = './src';
+const destPath = './resources';
+
+copyResourceFiles(srcPath, destPath);
