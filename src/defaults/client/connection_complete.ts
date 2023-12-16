98a246024312d04ff377d5a602052cbe25e4837f
Pablo Yuri <pabloyuridev@gmail.com>
2023-12-16 01:07:31 -0300

    feat: added new feats to connection complete

--- src/defaults/client/connection_complete.ts
+++ src/defaults/client/connection_complete.ts
@@ -1,7 +1,13 @@
 import * as alt from 'alt-client';
+import * as native from 'natives';
 // @ts-ignore
 import { loadMainInterface } from 'alt:webviews_handlers';
 
 alt.on('connectionComplete', async () => {
+  native.triggerScreenblurFadeIn(100);
+  native.displayRadar(false);
+  native.displayHud(false);
+  alt.showCursor(true);
+  alt.toggleGameControls(false);
   await loadMainInterface();
 });
