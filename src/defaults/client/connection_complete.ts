bc373703bbf521186926be21e76cbd40663eeca1
Pablo Yuri <pabloyuridev@gmail.com>
2023-12-16 01:23:29 -0300

    feat: added custom camera utils

--- src/defaults/client/connection_complete.ts
+++ src/defaults/client/connection_complete.ts
@@ -2,8 +2,13 @@ import * as alt from 'alt-client';
 import * as native from 'natives';
 // @ts-ignore
 import { loadMainInterface } from 'alt:webviews_handlers';
+// @ts-ignore
+import { createCustomCamera, renderCustomCamera, pointCustomCameraAtCoord } from 'alt:utils';
 
 alt.on('connectionComplete', async () => {
+  const signinCamera = createCustomCamera(new alt.Vector3(-485, 1095.75, 350), new alt.Vector3(0, 0, 0), 40);
+  pointCustomCameraAtCoord(signinCamera, new alt.Vector3(402.8664, -996.4108, -98.5));
+  renderCustomCamera(signinCamera);
   native.triggerScreenblurFadeIn(100);
   native.displayRadar(false);
   native.displayHud(false);
---
+++ src/utils/client/index.ts
@@ -0,0 +1,3 @@
+import { createCustomCamera, renderCustomCamera, pointCustomCameraAtCoord } from './camera_handler.js';
+
+export { createCustomCamera, renderCustomCamera, pointCustomCameraAtCoord };
