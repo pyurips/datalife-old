a702ecc99cb470414f26c8960290fd8a9977e371
Pablo Yuri <pabloyuridev@gmail.com>
2023-12-18 17:39:48 -0300

    feat: Added player modal and vehicle to test

--- src/auth/server/enter_game.ts
+++ src/auth/server/enter_game.ts
@@ -13,5 +13,7 @@ type IResponseData = {
 
 alt.on(`request:${EVENT_NAME}`, (player: alt.Player) => {
   player.emitRaw('request:auth_enterGame');
+  player.model = 0x90769A8F;
+  new alt.Vehicle(0x82E47E85, player.pos.x, player.pos.y, player.pos.z, 0, 0, 0);
   player.dimension = 0;
 });
