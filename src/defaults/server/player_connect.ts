1f9cd0316ee0415bed150eedaa0e8c480d1abcf0
Pablo Yuri <pabloyuridev@gmail.com>
2023-12-19 18:12:50 -0300

    hotfix

--- src/defaults/server/player_connect.ts
+++ src/defaults/server/player_connect.ts
@@ -3,6 +3,6 @@ import * as alt from 'alt-server';
 alt.on('playerConnect', (player) => {
   player.dimension = player.id + 1;
   player.spawn(-774.0126, 342.0428, 196.6864, 0);
-  player.model = 0x90769A8F;
-  player.giveWeapon(0x2B5EF5EC, 500, false);
+  player.model = 0x90769a8f;
+  player.giveWeapon(0x2b5ef5ec, 500, false);
 });
