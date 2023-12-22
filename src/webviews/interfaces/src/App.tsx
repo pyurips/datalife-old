701765a6320fe69153509d605705b55c383ca1b7
Pablo Yuri <pabloyuridev@gmail.com>
2023-12-22 02:08:49 -0300

    feat: added character creator screen

--- src/webviews/interfaces/src/App.tsx
+++ src/webviews/interfaces/src/App.tsx
@@ -1,8 +1,11 @@
+import CharacterCreator from './screens/character_creator';
 import Signin from './screens/signin';
 import { useState, useEffect } from 'react';
 
 export default function App() {
-  const [screen, setScreen] = useState<'signin' | ''>('signin');
+  const [screen, setScreen] = useState<'signin' | 'characterCreator' | ''>(
+    'characterCreator'
+  );
 
   useEffect(() => {
     // @ts-ignore
@@ -11,5 +14,10 @@ export default function App() {
       window.alt.on('response:webview_setScreen', (data) => setScreen(data));
   }, []);
 
-  return <>{screen === 'signin' && <Signin />}</>;
+  return (
+    <>
+      {screen === 'signin' && <Signin />}
+      {screen === 'characterCreator' && <CharacterCreator />}
+    </>
+  );
 }
---
+++ src/webviews/interfaces/src/screens/character_creator.tsx
@@ -0,0 +1,62 @@
+import { Input, Button, Link } from '@nextui-org/react';
+import { useEffect, useState } from 'react';
+
+export default function CharacterCreator() {
+  const [selectedMenu, setSelectedMenu] = useState<
+    | 'genetics'
+    | 'faceFeatures'
+    | 'hairAndFacialHair'
+    | 'personality'
+    | 'clothing'
+  >();
+
+  return (
+    <main className="flex h-screen w-full justify-end items-center px-7">
+      <div className="flex flex-row items-center justify-between bg-[#2D2D2D] w-[360px] h-[550px] rounded-2xl overflow-hidden">
+        <div>dsad</div>
+
+        <div className="flex flex-col justify-evenly items-center w-[100px] h-[90%]">
+          <Button
+            onClick={() => setSelectedMenu('genetics')}
+            isIconOnly
+            className={`flex rounded-xl w-[70px] h-[70px] bg-[#D9D9D9] ${
+              selectedMenu === 'genetics' ? 'opacity-100' : 'opacity-50'
+            }`}
+          ></Button>
+
+          <Button
+            onClick={() => setSelectedMenu('faceFeatures')}
+            isIconOnly
+            className={`flex rounded-xl w-[70px] h-[70px] bg-[#D9D9D9] ${
+              selectedMenu === 'faceFeatures' ? 'opacity-100' : 'opacity-50'
+            }`}
+          ></Button>
+
+          <Button
+            onClick={() => setSelectedMenu('hairAndFacialHair')}
+            isIconOnly
+            className={`flex rounded-xl w-[70px] h-[70px] bg-[#D9D9D9] ${
+              selectedMenu === 'hairAndFacialHair' ? 'opacity-100' : 'opacity-50'
+            }`}
+          ></Button>
+
+          <Button
+            onClick={() => setSelectedMenu('personality')}
+            isIconOnly
+            className={`flex rounded-xl w-[70px] h-[70px] bg-[#D9D9D9] ${
+              selectedMenu === 'personality' ? 'opacity-100' : 'opacity-50'
+            }`}
+          ></Button>
+
+          <Button
+            onClick={() => setSelectedMenu('clothing')}
+            isIconOnly
+            className={`flex rounded-xl w-[70px] h-[70px] bg-[#D9D9D9] ${
+              selectedMenu === 'clothing' ? 'opacity-100' : 'opacity-50'
+            }`}
+          ></Button>
+        </div>
+      </div>
+    </main>
+  );
+}
