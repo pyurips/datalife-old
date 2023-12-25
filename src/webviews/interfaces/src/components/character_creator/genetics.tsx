722738fc84195cffc800ec12012579d958c8e9ca
Pablo Yuri <pabloyuridev@gmail.com>
2023-12-25 09:56:14 -0300

    feat: added slider facial features

--- src/webviews/interfaces/src/components/character_creator/genetics.tsx
+++ src/webviews/interfaces/src/components/character_creator/genetics.tsx
@@ -2,11 +2,13 @@ import { Input, Button, Slider } from '@nextui-org/react';
 import { useEffect, useState } from 'react';
 import { FaMale, FaFemale } from 'react-icons/fa';
 import { RxUpdate } from 'react-icons/rx';
-import { MdFace, MdFace2 } from "react-icons/md";
+import { MdFace, MdFace2 } from 'react-icons/md';
 
 export default function Genetics() {
   const [characterName, setCharacterName] = useState<string>('');
   const [gender, setGender] = useState<boolean>(true);
+  const [selectedFace, setSelectedFace] = useState<number>(0);
+  const [facialFeatures, setFacialFeatures] = useState<number>(0.5);
 
   return (
     <div className="flex flex-1 p-5 flex-col gap-5 overflow-y-auto">
@@ -40,14 +42,20 @@ export default function Genetics() {
         <Slider
           startContent={<MdFace size={20} />}
           endContent={<MdFace2 size={20} />}
-          label="Rosto mais masculino"
+          label={
+            facialFeatures < 0.5
+              ? 'Rosto mais masculino'
+              : 'Rosto mais feminino'
+          }
           hideValue
+          value={facialFeatures}
+          onChange={(e) => setFacialFeatures(e as number)}
           step={0.01}
           maxValue={1}
           minValue={0}
           defaultValue={0.5}
           aria-label="Traços faciais"
-          color='foreground'
+          color="foreground"
         />
       </div>
 
@@ -59,22 +67,43 @@ export default function Genetics() {
           </Button>
         </div>
 
-        <div className="grid grid-cols-3 gap-4">
-          <div className="w-[50px] h-[50px] bg-gray-400"></div>
-          <div className="w-[50px] h-[50px] bg-gray-400"></div>
-          <div className="w-[50px] h-[50px] bg-gray-400"></div>
-          <div className="w-[50px] h-[50px] bg-gray-400"></div>
-          <div className="w-[50px] h-[50px] bg-gray-400"></div>
-          <div className="w-[50px] h-[50px] bg-gray-400"></div>
-          <div className="w-[50px] h-[50px] bg-gray-400"></div>
-          <div className="w-[50px] h-[50px] bg-gray-400"></div>
-          <div className="w-[50px] h-[50px] bg-gray-400"></div>
-          <div className="w-[50px] h-[50px] bg-gray-400"></div>
-          <div className="w-[50px] h-[50px] bg-gray-400"></div>
-          <div className="w-[50px] h-[50px] bg-gray-400"></div>
-          <div className="w-[50px] h-[50px] bg-gray-400"></div>
-          <div className="w-[50px] h-[50px] bg-gray-400"></div>
-          <div className="w-[50px] h-[50px] bg-gray-400"></div>
+        <div className="grid grid-cols-2 gap-4">
+          <Button
+            className={`flex flex-col w-[50px] h-[80px] p-2 bg-[#424242] ${
+              selectedFace === 0 ? 'opacity-100' : 'opacity-50'
+            }`}
+            onClick={() => setSelectedFace(0)}
+          >
+            <MdFace size={40} />
+            <p className="text-[10px]">Adjetivo</p>
+          </Button>
+          <Button
+            className={`flex flex-col w-[50px] h-[80px] p-2 bg-[#424242] ${
+              selectedFace === 1 ? 'opacity-100' : 'opacity-50'
+            }`}
+            onClick={() => setSelectedFace(1)}
+          >
+            <MdFace size={40} />
+            <p className="text-[10px]">Adjetivo</p>
+          </Button>
+          <Button
+            className={`flex flex-col w-[50px] h-[80px] p-2 bg-[#424242] ${
+              selectedFace === 2 ? 'opacity-100' : 'opacity-50'
+            }`}
+            onClick={() => setSelectedFace(2)}
+          >
+            <MdFace size={40} />
+            <p className="text-[10px]">Adjetivo</p>
+          </Button>
+          <Button
+            className={`flex flex-col w-[50px] h-[80px] p-2 bg-[#424242] ${
+              selectedFace === 3 ? 'opacity-100' : 'opacity-50'
+            }`}
+            onClick={() => setSelectedFace(3)}
+          >
+            <MdFace size={40} />
+            <p className="text-[10px]">Adjetivo</p>
+          </Button>
         </div>
       </div>
     </div>
