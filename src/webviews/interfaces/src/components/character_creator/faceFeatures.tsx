51b9b27f4dc25bd528755d9f5658c9164eaa29b5
Pablo Yuri <pabloyuridev@gmail.com>
2023-12-26 09:52:11 -0300

    feat: added eye colors

--- src/webviews/interfaces/src/components/character_creator/faceFeatures.tsx
+++ src/webviews/interfaces/src/components/character_creator/faceFeatures.tsx
@@ -1,11 +1,36 @@
 import { Input, Button, Slider } from '@nextui-org/react';
 import { useEffect, useState } from 'react';
+import eyeColors from '../../utils/eye_colors';
 
 export default function FaceFeatures() {
   return (
     <div className="flex flex-1 p-5 flex-col gap-5 overflow-y-auto">
       <div className="bg-[#424242] p-3 rounded-lg">
+        <p className="text-md">Tom de pele</p>
+        <Slider
+          size="sm"
+          step={0.01}
+          maxValue={1}
+          minValue={-1}
+          aria-label="Tamanho"
+          defaultValue={0.2}
+          color="foreground"
+        />
+      </div>
+
+      <div className="flex flex-col gap-2 bg-[#424242] p-3 rounded-lg">
         <p className="text-md">Cor dos olhos</p>
+        <div className="grid grid-cols-5 gap-4">
+          {eyeColors.map((e, i) => (
+            <Button
+              size="sm"
+              key={i}
+              style={{ backgroundColor: e.color }}
+              isIconOnly
+              onClick={() => {}}
+            ></Button>
+          ))}
+        </div>
       </div>
 
       <div className="flex flex-col gap-5 bg-[#424242] p-3 rounded-lg">
@@ -14,11 +39,21 @@ export default function FaceFeatures() {
           size="sm"
           step={0.01}
           maxValue={1}
-          minValue={0}
+          minValue={-1}
           label="Tamanho"
           aria-label="Tamanho"
           defaultValue={0.2}
-          color='foreground'
+          color="foreground"
+        />
+        <Slider
+          size="sm"
+          step={0.01}
+          maxValue={1}
+          minValue={-1}
+          label="Tamanho"
+          aria-label="Tamanho"
+          defaultValue={0.2}
+          color="foreground"
         />
       </div>
     </div>
---
+++ src/webviews/interfaces/src/utils/eye_colors.ts
@@ -0,0 +1,15 @@
+const eyeColors = [
+  { label: 'Preto profundo', value: 1, color: '#333333' }, // preto mais suave
+  { label: 'Azul/Verde gelo', value: 2, color: '#a0d8ef' }, // azul/verde claro mais suave
+  { label: 'Azul marinho', value: 3, color: '#27408b' }, // azul marinho mais suave
+  { label: 'Marrom terra', value: 4, color: '#b5651d' }, // marrom terra mais suave
+  { label: 'Marrom escuro', value: 5, color: '#8b4513' }, // marrom escuro mais suave
+  { label: 'Marrom claro', value: 6, color: '#deb887' }, // marrom claro mais suave
+  { label: 'Azul celeste', value: 7, color: '#5f9ea0' }, // azul mais suave
+  { label: 'Azul céu', value: 8, color: '#b0c4de' }, // azul claro mais suave
+  { label: 'Preto ônix', value: 12, color: '#363636' }, // preto novamente, mas mais suave
+  { label: 'Verde floresta', value: 13, color: '#228b22' }, // verde escuro mais suave
+  { label: 'Marrom amêndoa', value: 14, color: '#f4a460' }, // marrom claro novamente, mas mais suave
+];
+
+export default eyeColors;
