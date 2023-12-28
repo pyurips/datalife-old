282457dc4790f067a1b26b13860b49de358786d8
Pablo Yuri <pabloyuridev@gmail.com>
2023-12-28 17:20:59 -0300

    hotfix: personality

--- src/webviews/interfaces/src/components/character_creator/personality.tsx
+++ src/webviews/interfaces/src/components/character_creator/personality.tsx
@@ -1,3 +1,12 @@
 export default function Personality() {
-  return <div className="flex flex-1 p-5 flex-col gap-5 overflow-y-auto"></div>;
+  return (
+    <div className="flex flex-1 p-5 flex-col gap-5 overflow-y-auto">
+      <div className="w-full p-6 bg-[#424242] rounded-lg">
+        <p className="text-xs text-center text-neutral-400">
+          Nenhum traço selecionado
+        </p>
+      </div>
+      <p className="text-sm text-neutral-400">Selecione 3 traços</p>
+    </div>
+  );
 }
