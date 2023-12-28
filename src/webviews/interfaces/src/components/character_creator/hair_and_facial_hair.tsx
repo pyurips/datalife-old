2cdd807cc8b2a98b0ab5fb46a0ecd86af29697ef
Pablo Yuri <pabloyuridev@gmail.com>
2023-12-28 16:25:19 -0300

    feat: aded hair and facial hair screen medium configs

--- src/webviews/interfaces/src/components/character_creator/hair_and_facial_hair.tsx
+++ src/webviews/interfaces/src/components/character_creator/hair_and_facial_hair.tsx
@@ -1,5 +1,6 @@
 import { Tabs, Tab, Button } from '@nextui-org/react';
 import { maleHairNames } from '../../utils/hair_models';
+import hairColors from '../../utils/hair_colors';
 
 export default function HairAndFacialHair() {
   return (
@@ -18,7 +19,91 @@ export default function HairAndFacialHair() {
               ))}
             </div>
           </Tab>
-          <Tab key="color" title="Cores"></Tab>
+          <Tab key="color" title="Cores">
+            <div className="flex flex-col gap-4">
+              <h2 className="text-sm bg-[#424242] p-2 rounded-md">Primária</h2>
+              <div className="grid grid-cols-5 gap-4">
+                {hairColors.map((e, i) => (
+                  <Button
+                    style={{
+                      backgroundColor: e,
+                    }}
+                    key={i}
+                    size="sm"
+                    isIconOnly
+                    onClick={() => {}}
+                  ></Button>
+                ))}
+              </div>
+              <h2 className="text-sm bg-[#424242] p-2 rounded-md">
+                Secundária
+              </h2>
+              <div className="grid grid-cols-5 gap-4">
+                {hairColors.map((e, i) => (
+                  <Button
+                    style={{
+                      backgroundColor: e,
+                    }}
+                    key={i}
+                    size="sm"
+                    isIconOnly
+                    onClick={() => {}}
+                  ></Button>
+                ))}
+              </div>
+            </div>
+          </Tab>
+        </Tabs>
+      </div>
+
+      <div>
+        <Tabs aria-label="Options">
+          <Tab key="hair" title="Pelos faciais">
+            <div className="grid grid-cols-4 gap-4">
+              {maleHairNames.map((_, i) => (
+                <Button
+                  key={i}
+                  size="lg"
+                  isIconOnly
+                  onClick={() => {}}
+                ></Button>
+              ))}
+            </div>
+          </Tab>
+          <Tab key="color" title="Cores">
+            <div className="flex flex-col gap-4">
+              <h2 className="text-sm bg-[#424242] p-2 rounded-md">Primária</h2>
+              <div className="grid grid-cols-5 gap-4">
+                {hairColors.map((e, i) => (
+                  <Button
+                    style={{
+                      backgroundColor: e,
+                    }}
+                    key={i}
+                    size="sm"
+                    isIconOnly
+                    onClick={() => {}}
+                  ></Button>
+                ))}
+              </div>
+              <h2 className="text-sm bg-[#424242] p-2 rounded-md">
+                Secundária
+              </h2>
+              <div className="grid grid-cols-5 gap-4">
+                {hairColors.map((e, i) => (
+                  <Button
+                    style={{
+                      backgroundColor: e,
+                    }}
+                    key={i}
+                    size="sm"
+                    isIconOnly
+                    onClick={() => {}}
+                  ></Button>
+                ))}
+              </div>
+            </div>
+          </Tab>
         </Tabs>
       </div>
     </div>
