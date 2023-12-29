1265d177edb711d890d69c014a8ce3f88fb9ba26
Pablo Yuri <pabloyuridev@gmail.com>
2023-12-29 11:32:46 -0300

    personality screen initial configs

--- src/webviews/interfaces/src/components/character_creator/personality.tsx
+++ src/webviews/interfaces/src/components/character_creator/personality.tsx
@@ -1,3 +1,6 @@
+import { Button } from '@nextui-org/react';
+import { FaMasksTheater } from 'react-icons/fa6';
+
 export default function Personality() {
   return (
     <div className="flex flex-1 p-5 flex-col gap-5 overflow-y-auto">
@@ -7,6 +10,32 @@ export default function Personality() {
         </p>
       </div>
       <p className="text-sm text-neutral-400">Selecione 3 traços</p>
+      <div className="flex flex-col gap-4">
+        <Button size="lg" className="w-full">
+          <FaMasksTheater size={20} />
+          <p>Característica</p>
+        </Button>
+        <Button size="lg" className="w-full">
+          <FaMasksTheater size={20} />
+          <p>Característica</p>
+        </Button>
+        <Button size="lg" className="w-full">
+          <FaMasksTheater size={20} />
+          <p>Característica</p>
+        </Button>
+        <Button size="lg" className="w-full">
+          <FaMasksTheater size={20} />
+          <p>Característica</p>
+        </Button>
+        <Button size="lg" className="w-full">
+          <FaMasksTheater size={20} />
+          <p>Característica</p>
+        </Button>
+        <Button size="lg" className="w-full">
+          <FaMasksTheater size={20} />
+          <p>Característica</p>
+        </Button>
+      </div>
     </div>
   );
 }
