e16ee111c17f5ff6e17752c5610b0b0b2e75c9da
Pablo Yuri <pabloyuridev@gmail.com>
2024-01-02 17:16:02 -0300

    feat: added infos to debug hud

--- src/webviews/interfaces/src/screens/debug_hud.tsx
+++ src/webviews/interfaces/src/screens/debug_hud.tsx
@@ -1,4 +1,5 @@
 import datalifeLogoDark from '../assets/signin/datalife_logo_dark.svg';
+import { Button } from '@nextui-org/react';
 
 export default function DebugHud() {
   return (
@@ -11,6 +12,19 @@ export default function DebugHud() {
         />
         <p className="text-sm text-neutral-400">v0.01</p>
       </header>
+
+      <div className="flex flex-1 p-5 flex-col items-start">
+        <div className="flex flex-col bg-stone-800 rounded-lg p-2 gap-2">
+          <p className="text-sm">x: 1453.12</p>
+          <p className="text-sm">y: 1455.12</p>
+          <p className="text-sm">z: 7520.12</p>
+          <p className="text-sm">rot: 4455.12</p>
+          <p className="text-sm">spd: 4455.12</p>
+          <Button onPress={() => {}} variant="flat" color="primary">
+            Copiar coordenadas
+          </Button>
+        </div>
+      </div>
     </main>
   );
 }
