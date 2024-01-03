e81fb71f5b333d7f6adcc5da00e8dcea41938e62
Pablo Yuri <pabloyuridev@gmail.com>
2024-01-03 13:03:59 -0300

    hotfix: responsivity

--- src/webviews/interfaces/src/screens/debug_hud.tsx
+++ src/webviews/interfaces/src/screens/debug_hud.tsx
@@ -19,23 +19,25 @@ export default function DebugHud() {
 
   return (
     <main className="flex flex-col w-screen h-screen">
-      <header
-        className="flex flex-row w-full p-2 items-end gap-2"
-        style={{
-          transform: `scale(${scale})`,
-          marginLeft: 0.357 * window.innerWidth - 395.44,
-        }}
-      >
+      <header className="flex flex-row w-full items-end gap-[1vw] p-[0.5vw]">
         <img
           src={datalifeLogoDark}
           alt="DATALIFE logo dark"
-          className="w-[100px]"
+          className="w-[7vw]"
         />
-        <p className="text-sm text-neutral-400">v0.01</p>
+        <p className="text-[1vw] text-neutral-400">v0.01</p>
       </header>
 
-      <div className="flex flex-1 p-5 flex-col items-start">
-        <div className="flex flex-col bg-stone-800/[.9] rounded-lg p-2 gap-2">
+      <div className="flex flex-1 flex-col items-start" style={{
+        marginLeft: 0.056 * window.innerWidth - 67.52,
+        marginTop: 0.056 * window.innerWidth - 67.52
+      }}>
+        <div
+          className="flex flex-col bg-stone-800/[.9] rounded-lg p-2 gap-2"
+          style={{
+            transform: `scale(${scale})`,
+          }}
+        >
           <p className="text-sm">x: 1453.26</p>
           <p className="text-sm">y: 5436.30</p>
           <p className="text-sm">z: 7520.12</p>
