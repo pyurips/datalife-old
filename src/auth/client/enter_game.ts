d1d66c446bdcd05ffe339c6a94661e35a6d818b9
Pablo Yuri <pabloyuridev@gmail.com>
2024-01-04 21:34:17 -0300

    hotfix: request ipl fix

--- src/auth/client/enter_game.ts
+++ src/auth/client/enter_game.ts
@@ -1,10 +1,12 @@
 import * as alt from 'alt-client';
 // @ts-ignore
 import { setScreenMode } from 'alt:utils';
+import * as native from 'natives';
 
 const EVENT_NAME = 'auth_enterGame';
 
 alt.onceServer(`request:${EVENT_NAME}`, () => {
+  native.requestIpl('apa_v_mp_h_01_b');
   setScreenMode(false);
   alt.setMeta('currentScreen', 'debugHud');
   alt.emitRaw('request:auth_destroySigninCamera');
--- src/auth/server/enter_game.ts
+++ src/auth/server/enter_game.ts
@@ -5,4 +5,5 @@ const EVENT_NAME = 'auth_enterGame';
 alt.on(`request:${EVENT_NAME}`, (player: alt.Player) => {
   player.emitRaw('request:auth_enterGame');
   player.dimension = 0;
+  player.spawn(-774.0126, 342.0428, 196.6864, 0);
 });
