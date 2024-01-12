30b5d2e6d9d8e94f29fc68059bba3221b7368a34
Pablo Yuri <pabloyuridev@gmail.com>
2024-01-12 13:12:24 -0300

    feat: added all needs

--- src/webviews/main_interface/src/components/hud/need.tsx
+++ src/webviews/main_interface/src/components/hud/need.tsx
@@ -1,20 +1,35 @@
 import { CircularProgress } from '@nextui-org/react';
 import { ReactNode } from 'react';
 
-export default function Need({ icon, value }: { icon: ReactNode, value: number }) {
+export default function Need({
+  icon,
+  value,
+  color,
+}: {
+  icon: ReactNode;
+  value: number;
+  color:
+    | 'default'
+    | 'primary'
+    | 'secondary'
+    | 'success'
+    | 'warning'
+    | 'danger'
+    | undefined;
+}) {
   return (
     <div className="relative w-fit">
       <CircularProgress
         aria-label="Loading..."
         size="lg"
         value={value}
-        color="warning"
+        color={color}
         showValueLabel={false}
       />
 
       <div className="flex items-center justify-center absolute top-0 left-0 w-full h-full rounded-[100%]">
         <div className="flex items-center justify-center w-[30px] h-[30px] rounded-[100%]">
-          { icon }
+          {icon}
         </div>
       </div>
     </div>
--- src/webviews/main_interface/src/screens/hud.tsx
+++ src/webviews/main_interface/src/screens/hud.tsx
@@ -1,10 +1,16 @@
 import Need from '../components/hud/need';
 import { PiBowlFoodFill } from 'react-icons/pi';
+import { BiSolidDrink } from 'react-icons/bi';
+import { FaToiletPaper } from 'react-icons/fa6';
+import { IoBedSharp } from 'react-icons/io5';
 
 export default function Hud() {
   return (
     <main className="flex flex-col w-screen h-screen">
-      <Need icon={<PiBowlFoodFill size={25} />} value={50} />
+      <Need color="warning" icon={<PiBowlFoodFill size={25} />} value={50} />
+      <Need color="primary" icon={<BiSolidDrink size={25} />} value={35} />
+      <Need color="secondary" icon={<FaToiletPaper size={25} />} value={35} />
+      <Need color="default" icon={<IoBedSharp size={25} />} value={35} />
     </main>
   );
 }
