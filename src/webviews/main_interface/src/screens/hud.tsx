664a50fa5a9ca8198410a2b7db081c9599a19f50
Pablo Yuri <pabloyuridev@gmail.com>
2024-01-12 14:40:29 -0300

    feat: hud handle resize added

--- src/webviews/main_interface/src/screens/hud.tsx
+++ src/webviews/main_interface/src/screens/hud.tsx
@@ -3,8 +3,24 @@ import { PiBowlFoodFill } from 'react-icons/pi';
 import { BiSolidDrink } from 'react-icons/bi';
 import { FaToiletPaper } from 'react-icons/fa6';
 import { IoBedSharp } from 'react-icons/io5';
+import { useEffect, useState } from 'react';
+import datalifeLogoDark from '../assets/signin/datalife_logo_dark.svg';
 
 export default function Hud() {
+  const [scale, setScale] = useState((window.innerWidth + 520) / 1886.6);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setScale((window.innerWidth + 520) / 1886.6);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <main className="flex flex-col w-screen h-screen">
       <div className="flex flex-row gap-1">
