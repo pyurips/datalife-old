6c8e6048351d75c11fc543cec17f7330a377db55
Pablo Yuri <pabloyuridev@gmail.com>
2024-01-12 13:27:15 -0300

    hotfix: hud

--- src/webviews/main_interface/src/screens/hud.tsx
+++ src/webviews/main_interface/src/screens/hud.tsx
@@ -7,10 +7,28 @@ import { IoBedSharp } from 'react-icons/io5';
 export default function Hud() {
   return (
     <main className="flex flex-col w-screen h-screen">
-      <Need color="warning" icon={<PiBowlFoodFill size={25} />} value={50} />
-      <Need color="primary" icon={<BiSolidDrink size={25} />} value={35} />
-      <Need color="secondary" icon={<FaToiletPaper size={25} />} value={35} />
-      <Need color="default" icon={<IoBedSharp size={25} />} value={35} />
+      <div className="flex flex-row gap-1">
+        <Need
+          color="warning"
+          icon={<PiBowlFoodFill size={25} color="#f5a524" />}
+          value={50}
+        />
+        <Need
+          color="primary"
+          icon={<BiSolidDrink size={25} color="#006FEE" />}
+          value={35}
+        />
+        <Need
+          color="secondary"
+          icon={<FaToiletPaper size={25} color="#9353d3" />}
+          value={35}
+        />
+        <Need
+          color="default"
+          icon={<IoBedSharp size={25} color="#3f3f46" />}
+          value={35}
+        />
+      </div>
     </main>
   );
 }
