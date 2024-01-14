230207676efbaf597d66e77cc507c51cdf1798b4
Pablo Yuri <pabloyuridev@gmail.com>
2024-01-13 21:44:44 -0300

    feat: added logo to hud

--- src/webviews/main_interface/src/screens/hud.tsx
+++ src/webviews/main_interface/src/screens/hud.tsx
@@ -23,11 +23,30 @@ export default function Hud() {
 
   return (
     <main className="flex flex-col w-screen h-screen">
-      <div style={{
-        right: 0.06 * window.innerWidth - 65.2,
-        top: 0.018 * window.innerWidth - 9.4,
-        transform: `scale(${scale})`,
-      }} className="absolute flex flex-row gap-1">
+      <div
+        className="absolute flex flex-row items-end gap-2"
+        style={{
+          left: 0.06 * window.innerWidth - 65.2,
+          top: 0.018 * window.innerWidth - 9.4,
+          transform: `scale(${scale})`,
+        }}
+      >
+        <img
+          src={datalifeLogoDark}
+          alt="DATALIFE logo dark"
+          className="w-[100px]"
+        />
+        <p className="text-[12px] text-neutral-400">v0.01</p>
+      </div>
+
+      <div
+        style={{
+          right: 0.06 * window.innerWidth - 65.2,
+          top: 0.018 * window.innerWidth - 9.4,
+          transform: `scale(${scale})`,
+        }}
+        className="absolute flex flex-row gap-1"
+      >
         <Need
           color="warning"
           icon={<PiBowlFoodFill size={25} color="#f5a524" />}
