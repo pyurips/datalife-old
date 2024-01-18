f4a2dbad956787f2832512e094a2adf0eca3b786
Pablo Yuri <pabloyuridev@gmail.com>
2024-01-18 14:27:55 -0300

    hotfix

--- src/main/client/character/ped_in_creator_handler.ts
+++ src/main/client/character/ped_in_creator_handler.ts
@@ -2,15 +2,10 @@ import * as alt from 'alt-client';
 import * as native from 'natives';
 
 let pedInCreator = null;
-let pedInCreatorModel = Math.random() > 0.5 ? 0x705e61f2 : 0x9c9effd8;
-let pedInCreatorHeadBlend = null;
-let pedInCreatorEyeColor = null;
-let pedInCreatorHair = null;
-let pedInCreatorHairColors = { primary: null, secondary: null };
 
 export async function createPedInCreator() {
   pedInCreator = new alt.LocalPed(
-    pedInCreatorModel,
+    0x705e61f2,
     alt.Player.local.dimension,
     new alt.Vector3(-763.17, 330.59, 199.49),
     new alt.Vector3(0, 0, -3.06)
