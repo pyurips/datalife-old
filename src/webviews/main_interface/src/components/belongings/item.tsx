537fce8c279d2ce9a98c3251e0d27e58ebeda1bd
Pablo Yuri <pabloyuridev@gmail.com>
2024-01-31 01:42:13 -0300

    feat: Added dropdown

--- src/webviews/main_interface/src/components/belongings/item.tsx
+++ src/webviews/main_interface/src/components/belongings/item.tsx
@@ -1,7 +1,31 @@
-import { Button } from "@nextui-org/react";
+import {
+  Button,
+  Dropdown,
+  DropdownTrigger,
+  DropdownMenu,
+  DropdownItem,
+} from "@nextui-org/react";
 
 export default function Item() {
-  return <Button className="w-[80px] h-[80px] bg-stone-900">
-
-  </Button>;
+  return (
+    <Dropdown className="dark">
+      <DropdownTrigger>
+        <Button className="w-[80px] h-[80px] bg-stone-900"></Button>
+      </DropdownTrigger>
+      <DropdownMenu className="dark" aria-label="Static Actions">
+        <DropdownItem key="new" className="text-stone-300">
+          New file
+        </DropdownItem>
+        <DropdownItem key="copy" className="text-stone-300">
+          Copy link
+        </DropdownItem>
+        <DropdownItem key="edit" className="text-stone-300">
+          Edit file
+        </DropdownItem>
+        <DropdownItem key="delete" className="text-danger" color="danger">
+          Delete file
+        </DropdownItem>
+      </DropdownMenu>
+    </Dropdown>
+  );
 }
