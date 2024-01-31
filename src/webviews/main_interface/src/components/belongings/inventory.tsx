e32838fe83427cbdefb5cccff21005d53fb43b15
Pablo Yuri <pabloyuridev@gmail.com>
2024-01-31 16:23:44 -0300

    feat: weight inventory added

--- src/webviews/main_interface/src/components/belongings/inventory.tsx
+++ src/webviews/main_interface/src/components/belongings/inventory.tsx
@@ -1,16 +1,29 @@
-import { Input } from "@nextui-org/react";
-import { FaSearch } from "react-icons/fa";
+import { Input, Progress } from "@nextui-org/react";
+import { FaSearch, FaWeightHanging } from "react-icons/fa";
 import Item from "./item";
 
 export default function Inventory() {
   return (
     <div className="flex flex-col gap-3 w-full">
-      <Input size="sm" variant="underlined" startContent={<FaSearch size={20} />} />
+      <Input
+        size="sm"
+        variant="underlined"
+        startContent={<FaSearch size={20} />}
+      />
 
       <div className="flex flex-row gap-4 flex-wrap overflow-auto">
         {new Array(50).fill(0).map((_) => (
           <Item />
         ))}
+      </div>
+
+      <div className="flex flex-row items-center justify-between gap-3">
+        <FaWeightHanging />
+        <Progress size="sm" color="secondary" aria-label="Loading..." value={70} />
+        <div className="flex flex-row gap-1 items-center">
+          <p className="text-stone-300 text-sm">112/150</p>
+          <p className="text-stone-400 text-xs">kg</p>
+        </div>
       </div>
     </div>
   );
