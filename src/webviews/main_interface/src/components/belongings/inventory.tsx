1f8c00ed2dc531adb4a2e1d5383dca1caa91a072
Pablo Yuri <pabloyuridev@gmail.com>
2024-01-31 02:16:27 -0300

    hotfix: item

--- src/webviews/main_interface/src/components/belongings/inventory.tsx
+++ src/webviews/main_interface/src/components/belongings/inventory.tsx
@@ -5,7 +5,7 @@ import Item from "./item";
 export default function Inventory() {
   return (
     <div className="flex flex-col gap-3 w-full">
-      <Input size="sm" startContent={<FaSearch size={20} />} />
+      <Input size="sm" variant="underlined" startContent={<FaSearch size={20} />} />
 
       <div className="flex flex-row gap-4 flex-wrap overflow-auto">
         {new Array(50).fill(0).map((_) => (
