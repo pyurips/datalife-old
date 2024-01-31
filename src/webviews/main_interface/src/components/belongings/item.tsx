e46a53da04036f5ca73fb2a9ca4142db571a2829
Pablo Yuri <pabloyuridev@gmail.com>
2024-01-31 02:13:32 -0300

    hotfix: item

--- src/webviews/main_interface/src/components/belongings/item.tsx
+++ src/webviews/main_interface/src/components/belongings/item.tsx
@@ -1,31 +1,28 @@
 import {
   Button,
-  Dropdown,
-  DropdownTrigger,
-  DropdownMenu,
-  DropdownItem,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  Divider
 } from "@nextui-org/react";
 
 export default function Item() {
   return (
-    <Dropdown className="dark">
-      <DropdownTrigger>
+    <Popover className="dark">
+      <PopoverTrigger>
         <Button className="w-[80px] h-[80px] bg-stone-900"></Button>
-      </DropdownTrigger>
-      <DropdownMenu className="dark" aria-label="Static Actions">
-        <DropdownItem key="new" className="text-stone-300">
-          New file
-        </DropdownItem>
-        <DropdownItem key="copy" className="text-stone-300">
-          Copy link
-        </DropdownItem>
-        <DropdownItem key="edit" className="text-stone-300">
-          Edit file
-        </DropdownItem>
-        <DropdownItem key="delete" className="text-danger" color="danger">
-          Delete file
-        </DropdownItem>
-      </DropdownMenu>
-    </Dropdown>
+      </PopoverTrigger>
+      <PopoverContent>
+        <div className="flex flex-col gap-3 p-2 max-w-[200px]">
+          <div className="text-sm text-stone-300 text-justify">Nome do item dsa da da dasda dasad dasda weerr</div>
+          <div className="text-xs text-stone-400 text-justify">Descrição do item das dad as da dadas da da das da dad ad a</div>
+          <Divider />
+          <Button size="sm" variant="light" className="justify-start">Botão 1</Button>
+          <Button size="sm" variant="light" className="justify-start">Botão 1</Button>
+          <Button size="sm" variant="light" className="justify-start">Botão 1</Button>
+          <Button size="sm" variant="light" className="justify-start">Botão 1</Button>
+        </div>
+      </PopoverContent>
+    </Popover>
   );
 }
