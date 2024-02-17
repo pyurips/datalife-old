677523a4167ebeeabeaaf2ea53c2baa1e1dbeaf4
Pablo Yuri <pabloyuridev@gmail.com>
2024-02-17 20:02:47 -0300

    feat: added theme provider

--- src/webviews/main_interface/src/App.tsx
+++ src/webviews/main_interface/src/App.tsx
@@ -1,17 +1,20 @@
-import CharacterMenu from "./pages/character_menu";
-import MainHud from "./pages/main_hud";
-import Signin from "./pages/signin";
-import { useListener } from "./utils/use_listener";
+import CharacterMenu from './pages/character_menu';
+import MainHud from './pages/main_hud';
+import Signin from './pages/signin';
+import { useListener } from './utils/use_listener';
+import { ThemeProvider } from './components/theme_provider';
 
 export default function App() {
-  const screen = useListener("webView_setScreen");
+  const screen = useListener('webView_setScreen');
 
   return (
-    <main className="h-screen w-screen flex items-center justify-center">
-      {screen === "signin" && <Signin />}
-      {screen === "mainHud" && <MainHud />}
-      {screen === "characterMenu" && <CharacterMenu />}
-      <CharacterMenu />
-    </main>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <main className="h-screen w-screen flex items-center justify-center">
+        {screen === 'signin' && <Signin />}
+        {screen === 'mainHud' && <MainHud />}
+        {screen === 'characterMenu' && <CharacterMenu />}
+        <CharacterMenu />
+      </main>
+    </ThemeProvider>
   );
 }
