a084111d0c5f78c68e54a7f6d3595aba70cc23fc
Pablo Yuri <pabloyuridev@gmail.com>
2024-02-17 21:37:48 -0300

    hotfix

--- src/webviews/main_interface/src/pages/character_customization.tsx
+++ src/webviews/main_interface/src/pages/character_customization.tsx
@@ -5,20 +5,32 @@ export default function CharacterCustomization() {
   return (
     <main className="flex flex-row items-center justify-end w-full h-full p-[1vw]">
       <div className="flex flex-row justify-between w-[25vw] h-[40vw] bg-stone-950 rounded-[1vw] overflow-hidden">
-        <div className="flex flex-1 flex-col gap-[1vw] p-[1vw]">
-          <p className="text-[1vw]">Nome do personagem no discord</p>
-          <div className="flex flex-row gap-[1vw]">
-            <Button variant="secondary" className="size-[3vw]"></Button>
-            <Button variant="secondary" className="size-[3vw]"></Button>
-          </div>
+        <ScrollArea className="flex flex-1">
+          <div className="flex flex-col gap-[1vw] p-[1vw]">
+            <p className="text-[1vw]">Nome do personagem no discord</p>
+            <div className="flex flex-row gap-[1vw]">
+              <Button variant="secondary" className="size-[3vw]"></Button>
+              <Button variant="secondary" className="size-[3vw]"></Button>
+            </div>
 
-          <div className="flex flex-row items-center gap-[1vw]">
-            <Button variant="ghost"></Button>
-            <p className="text-[0.9vw] text-stone-400">
-              Clique para gerar novos rostos
-            </p>
+            <div className="flex flex-row items-center gap-[1vw]">
+              <Button className='size-[2vw]' variant="ghost"></Button>
+              <p className="text-[0.9vw] text-stone-400">
+                Clique para gerar novos rostos
+              </p>
+            </div>
+
+            <div className="flex flex-row gap-[1vw] flex-wrap">
+              {new Array(50).fill(0).map((_, i) => (
+                <Button
+                  key={i}
+                  variant="secondary"
+                  className="size-[3vw]"
+                ></Button>
+              ))}
+            </div>
           </div>
-        </div>
+        </ScrollArea>
 
         <ScrollArea>
           <div className="flex flex-col gap-[1vw] p-[1vw]">
