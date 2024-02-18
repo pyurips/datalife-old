a83a6fd083856151dbcec244034a5dbd0c4bc058
Pablo Yuri <pabloyuridev@gmail.com>
2024-02-17 22:14:59 -0300

    feat: genetics added

---
+++ src/webviews/main_interface/src/components/character_customization/genetics.tsx
@@ -0,0 +1,29 @@
+import { ScrollArea } from '@/components/ui/scroll-area';
+import { Button } from '@/components/ui/button';
+
+export default function Genetics() {
+  return (
+    <ScrollArea className="flex flex-1">
+      <div className="flex flex-col gap-[1vw] p-[1vw]">
+        <p className="text-[1vw]">Nome do personagem no discord</p>
+        <div className="flex flex-row gap-[1vw]">
+          <Button size="icon" variant="secondary" className="size-[3vw]"></Button>
+          <Button size="icon" variant="secondary" className="size-[3vw]"></Button>
+        </div>
+
+        <div className="flex flex-row items-center gap-[1vw]">
+          <Button size="icon" className="size-[2vw]" variant="ghost"></Button>
+          <p className="text-[0.9vw] text-stone-400">
+            Clique para gerar novos rostos
+          </p>
+        </div>
+
+        <div className="flex flex-row gap-[1vw] flex-wrap">
+          {new Array(50).fill(0).map((_, i) => (
+            <Button size="icon" key={i} variant="secondary" className="size-[3vw]"></Button>
+          ))}
+        </div>
+      </div>
+    </ScrollArea>
+  );
+}
--- src/webviews/main_interface/src/pages/character_customization.tsx
+++ src/webviews/main_interface/src/pages/character_customization.tsx
@@ -1,48 +1,33 @@
+import Genetics from '@/components/character_customization/genetics';
 import { Button } from '@/components/ui/button';
 import { ScrollArea } from '@/components/ui/scroll-area';
 
 export default function CharacterCustomization() {
   return (
     <main className="flex flex-row items-center justify-end w-full h-full p-[1vw]">
-      <div className="flex flex-row justify-between w-[25vw] h-[40vw] bg-stone-950 rounded-[1vw] overflow-hidden">
-        <ScrollArea className="flex flex-1">
-          <div className="flex flex-col gap-[1vw] p-[1vw]">
-            <p className="text-[1vw]">Nome do personagem no discord</p>
-            <div className="flex flex-row gap-[1vw]">
-              <Button variant="secondary" className="size-[3vw]"></Button>
-              <Button variant="secondary" className="size-[3vw]"></Button>
-            </div>
+      <div className="flex flex-col gap-[1vw]">
+        <div className="flex flex-row justify-between w-[25vw] h-[40vw] bg-stone-950 rounded-[1vw] overflow-hidden">
+          <Genetics />
 
-            <div className="flex flex-row items-center gap-[1vw]">
-              <Button className='size-[2vw]' variant="ghost"></Button>
-              <p className="text-[0.9vw] text-stone-400">
-                Clique para gerar novos rostos
-              </p>
-            </div>
-
-            <div className="flex flex-row gap-[1vw] flex-wrap">
-              {new Array(50).fill(0).map((_, i) => (
+          <ScrollArea>
+            <div className="flex flex-col gap-[1vw] p-[1vw]">
+              {new Array(20).fill(0).map((_, i) => (
                 <Button
+                  size="icon"
                   key={i}
+                  className="size-[4vw]"
                   variant="secondary"
-                  className="size-[3vw]"
                 ></Button>
               ))}
             </div>
-          </div>
-        </ScrollArea>
+          </ScrollArea>
+        </div>
 
-        <ScrollArea>
-          <div className="flex flex-col gap-[1vw] p-[1vw]">
-            {new Array(20).fill(0).map((_, i) => (
-              <Button
-                key={i}
-                className="size-[4vw]"
-                variant="secondary"
-              ></Button>
-            ))}
-          </div>
-        </ScrollArea>
+        <div className="flex flex-row items-center justify-between">
+          <Button className="bg-green-600 hover:bg-green-700 text-[1vw] py-[1vw] px-[1.1vw] rounded-[0.5vw]">
+            Finalizar
+          </Button>
+        </div>
       </div>
     </main>
   );
