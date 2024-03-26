cbe160f413f368b04999fa643866630fd652dcee
Pablo Yuri <pabloyuridev@gmail.com>
2024-03-26 18:58:51 -0300

    feat: add admin panel cateories and options

--- src/webviews/main_interface/src/pages/admin_panel.tsx
+++ src/webviews/main_interface/src/pages/admin_panel.tsx
@@ -1,26 +1,53 @@
 import { Button } from '@/components/ui/button';
 import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area';
+import adminCategories from '@/utils/admin_panel_categories';
+import { useState } from 'react';
 
 export default function AdminPanel() {
+  const [selectedCategory, setSelectedCategory] = useState(
+    adminCategories[0].category
+  );
+  const [selectedOption, setSelectedOption] = useState(
+    adminCategories[0].options[0]
+  );
+
   return (
     <div className="flex flex-col w-[50vw] h-[30vw] bg-stone-950 rounded-[1vw] overflow-hidden">
       <ScrollArea>
         <div className="flex w-full p-[1vw] flex-row gap-[1vw] overflow-auto">
-          <Button className="p-[1vw] h-min">das</Button>
-          <Button className="p-[1vw] h-min">das</Button>
-          <Button className="p-[1vw] h-min">das</Button>
-          <Button className="p-[1vw] h-min">das</Button>
-          <Button className="p-[1vw] h-min">das</Button>
-          <Button className="p-[1vw] h-min">das</Button>
-          <Button className="p-[1vw] h-min">das</Button>
-          <Button className="p-[1vw] h-min">das</Button>
-          <Button className="p-[1vw] h-min">das</Button>
-          <Button className="p-[1vw] h-min">das</Button>
-          <Button className="p-[1vw] h-min">das</Button>
-          <Button className="p-[1vw] h-min">das</Button>
+          {adminCategories.map(({ category }) => (
+            <Button
+              key={category}
+              className={`p-[1vw] h-min ${
+                selectedCategory === category ? 'opacity-100' : 'opacity-50'
+              }`}
+              onClick={() => setSelectedCategory(category)}
+            >
+              {category}
+            </Button>
+          ))}
           <ScrollBar orientation="horizontal" />
         </div>
       </ScrollArea>
+
+      <div className="flex flex-1 flex-row overflow-auto">
+        <div className="flex flex-col gap-3 p-3 overflow-auto h-full">
+          {adminCategories
+            .find(({ category }) => category === selectedCategory)
+            ?.options.map((option) => (
+              <Button
+                variant="ghost"
+                key={option}
+                className={`p-[1vw] h-min ${
+                  selectedOption === option ? 'opacity-100' : 'opacity-50'
+                }`}
+                onClick={() => setSelectedOption(option)}
+              >
+                {option}
+              </Button>
+            ))}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/webviews/main_interface/src/utils/admin_panel_categories.ts
@@ -0,0 +1,20 @@
+const adminCategories = [
+  {
+    category: 'Mundo',
+    options: ['Teleporte'],
+  },
+  {
+    category: 'Jogadores',
+    options: ['Banir', 'Desbanir', 'Kickar'],
+  },
+  {
+    category: 'Servidor',
+    options: ['Reiniciar', 'Desligar'],
+  },
+  {
+    category: 'Veículos',
+    options: ['Spawnar', 'Remover'],
+  },
+];
+
+export default adminCategories;
