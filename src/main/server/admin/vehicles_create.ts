c3b7ff975a3f07e4a40aba742d93315ba4e28995
Pablo Yuri <pabloyuridev@gmail.com>
2024-03-30 21:05:31 -0300

    feat: vehicle spawn sen and cos

--- src/main/server/admin/vehicles_create.ts
+++ src/main/server/admin/vehicles_create.ts
@@ -9,14 +9,23 @@ export async function admin_vehicles_createToMe(
   try {
     const accountData = player.getLocalMeta('accountData') as any;
     if (accountData.permission_level < 2) throw sendClientError(1711835426);
-    createAVehicle(data.modelId, player.pos, player.rot, {
-      fuelType: 'gasoline',
-      fuel: 5000,
-      fuelRate: 1,
-      numberPlateStyle: 0,
-      numberPlateText: 'Pabyu',
-      locked: 1,
-    });
+    createAVehicle(
+      data.modelId,
+      new alt.Vector3(
+        player.pos.x + Math.cos(player.rot.z)*2,
+        player.pos.y + Math.sin(player.rot.z)*2,
+        player.pos.z
+      ),
+      player.rot,
+      {
+        fuelType: 'gasoline',
+        fuel: 5000,
+        fuelRate: 1,
+        numberPlateStyle: 0,
+        numberPlateText: 'Pabyu',
+        locked: 1,
+      }
+    );
   } catch (e) {
     if (e.name === 'DATALIFEClientError') throw e;
     throw sendClientError(1711835420);
