5cfb732d1ed8b5f82836e20cb5b39742a6cc4ff8
Pablo Yuri <pabloyuridev@gmail.com>
2024-04-02 09:00:15 -0300

    feat: add webview class configs

--- src/main/client/utils.ts
+++ src/main/client/utils.ts
@@ -1,3 +1,23 @@
-class Utils {}
+import * as alt from 'alt-client';
+
+class Utils {
+  static cursorCount = 0;
+
+  static showCursor(state: boolean) {
+    if (state) {
+      this.cursorCount += 1;
+      try {
+        alt.showCursor(true);
+      } catch (_) {}
+    } else {
+      for (let i = 0; i < this.cursorCount; i++) {
+        try {
+          alt.showCursor(false);
+        } catch (_) {}
+      }
+      this.cursorCount = 0;
+    }
+  }
+}
 
 export default Utils;
--- src/main/client/webview.ts
+++ src/main/client/webview.ts
@@ -1,3 +1,33 @@
-class Webview {}
+import * as alt from 'alt-client';
+import requester from './requester.js';
+
+class Webview {
+  static webViews = ['http://assets/webviews/main_interface/index.html'];
+  static activeWebViews: alt.WebView[] = [];
+
+  static async loadWebView(WebviewId: number, isOverlay = false) {
+    const webView = new alt.WebView(this.webViews[WebviewId], isOverlay);
+
+    webView.on('request', async (operationName: string, data?: unknown) => {
+      try {
+        const response = requester(operationName, data);
+        webView.emit(`response:${operationName}`, response);
+      } catch (error) {
+        webView.emit(`response:${operationName}`, {
+          error,
+        });
+      }
+    });
+
+    await new Promise((resolve) => {
+      webView.once('load', resolve);
+    });
+  }
+
+  static toggleFocus(webViewId: number, state: boolean) {
+    if (state) return this.activeWebViews[webViewId].focus();
+    return this.activeWebViews[webViewId].unfocus();
+  }
+}
 
 export default Webview;
