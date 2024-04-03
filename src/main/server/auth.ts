bdeab15d32d742579173b8d467b13d8feff52179
Pablo Yuri <pabloyuridev@gmail.com>
2024-04-03 02:23:07 -0300

    hotfix

--- src/main/server/auth.ts
+++ src/main/server/auth.ts
@@ -11,26 +11,32 @@ class Auth {
   }
 
   async signin(data: { token: string }) {
-    const response = await axios.get('https://discordapp.com/api/users/@me', {
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        Authorization: `Bearer ${data.token}`,
-      },
-    });
-    if (!response || !response.data.id) throw Utils.sendClientError(1705460913);
-    const pool = new Postgresql();
-    const accountData = await pool.signinByAccount(response.data.id);
-    new Account(
-      this.player,
-      accountData.id,
-      accountData.discord_id,
-      accountData.created_at,
-      accountData.updated_at,
-      accountData.last_login,
-      accountData.permission_level,
-      accountData.bits
-    );
-    return 1;
+    try {
+      const response = await axios.get('https://discordapp.com/api/users/@me', {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          Authorization: `Bearer ${data.token}`,
+        },
+      });
+      if (!response || !response.data.id)
+        throw Utils.sendClientError(1705460913);
+      const pool = new Postgresql();
+      const accountData = await pool.signinByAccount(response.data.id);
+      new Account(
+        this.player,
+        accountData.id,
+        accountData.discord_id,
+        accountData.created_at,
+        accountData.updated_at,
+        accountData.last_login,
+        accountData.permission_level,
+        accountData.bits
+      );
+      return 1;
+    } catch (e) {
+      if (e.name === 'DATALIFEClientError') throw e;
+      throw Utils.sendClientError(1705460706);
+    }
   }
 
   public callableByRPC = {
