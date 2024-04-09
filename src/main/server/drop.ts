152a2a22baae6856c896e830715e97e06ac1520d
Pablo Yuri <pabloyuridev@gmail.com>
2024-04-09 12:04:34 -0300

    hotfix: drop class

--- src/main/server/drop.ts
+++ src/main/server/drop.ts
@@ -2,25 +2,31 @@ import * as alt from 'alt-server';
 
 class Drop {
   public position: alt.Vector3;
-  public objectId: number;
-  public objectWeight: number;
-  public objectQuantity: number;
-  public objectQuality: 0 | 1 | 2;
+  public itemId: number;
+  public itemWeight: number;
+  public itemQuantity: number;
+  public itemQuality: 0 | 1 | 2;
   public createdAt: number;
   public dropInstance: alt.Object;
 
   constructor(
-    objectId: number,
-    objectWeight: number,
-    objectQuantity: number,
-    objectQuality: 0 | 1 | 2
+    itemId: number,
+    itemWeight: number,
+    itemQuantity: number,
+    itemQuality: 0 | 1 | 2,
+    position: alt.Vector3
   ) {
-    objectId = this.objectId;
-    objectWeight = this.objectWeight;
-    objectQuantity = this.objectQuantity;
-    objectQuality = this.objectQuality;
+    itemId = this.itemId;
+    itemWeight = this.itemWeight;
+    itemQuantity = this.itemQuantity;
+    itemQuality = this.itemQuality;
+    position = this.position;
     this.createdAt = Date.now();
-    // this.dropInstance = new alt.Object();
+    this.dropInstance = new alt.Object(
+      'prop_cs_box_clothes',
+      position,
+      new alt.Vector3(0, 0, 0)
+    );
   }
   public delete() {}
   static getDropIdByInstance() {}
