90c39171ecc88c6aa9d42e6d79ef31eba2123098
Pablo Yuri <pabloyuridev@gmail.com>
2024-04-09 14:29:28 -0300

    feat: add vehicle type method

--- src/main/client/vehicle.ts
+++ src/main/client/vehicle.ts
@@ -1,4 +1,5 @@
 import * as alt from 'alt-client';
+import * as native from 'natives';
 
 class Vehicle {
   static vehicleInstance = alt.Player.local.vehicle;
@@ -9,6 +10,17 @@ class Vehicle {
 
   static gear() {
     return this.vehicleInstance.gear;
+  }
+
+  static type() {
+    if (native.isThisModelABicycle(this.vehicleInstance.model)) return 'bicycle';
+    if (native.isThisModelABike(this.vehicleInstance.model)) return 'bike';
+    if (native.isThisModelABoat(this.vehicleInstance.model)) return 'boat';
+    if (native.isThisModelACar(this.vehicleInstance.model)) return 'car';
+    if (native.isThisModelAHeli(this.vehicleInstance.model)) return 'heli';
+    if (native.isThisModelAPlane(this.vehicleInstance.model)) return 'plane';
+    if (native.isThisModelAQuadbike(this.vehicleInstance.model)) return 'quadbike';
+    return null;
   }
 }
 
