6dd8a69fa9e98756b7fd7f468c134ba2ac36c32b
Pablo Yuri <pabloyuridev@gmail.com>
2024-04-15 05:05:30 -0300

    feat: add shadcn to other vehicle hud status webview

--- src/webviews/vehicle_status_interface/vite.config.ts
+++ src/webviews/vehicle_status_interface/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,5 +8,10 @@ export default defineConfig({
   base: "./",
   build: {
     outDir: "../../../resources/webviews/vehicle_status_interface",
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 })
