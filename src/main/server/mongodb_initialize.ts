ae67baad5d28b6d93101404b30a5a515c3e9b360
Pablo Yuri <pabloyuridev@gmail.com>
2024-04-19 12:11:23 -0300

    hotfix

--- src/main/server/mongodb_initialize.ts
+++ src/main/server/mongodb_initialize.ts
@@ -73,23 +73,31 @@ export const characterModel = model<CharacterData>(
 
 export function initializeMongoDBCore() {
   const MONGODB_URI = process.env.MONGODB_URI;
+  if (!MONGODB_URI) throw new Error('MONGODB_URI is not set');
   mongoDBCoreInstance = createConnection(MONGODB_URI);
   mongoDBCoreInstance.useDb('core');
 }
 
 export function initializeMongoDBGame() {
   const MONGODB_URI = process.env.MONGODB_URI;
+  if (!MONGODB_URI) throw new Error('MONGODB_URI is not set');
   mongoDBGameInstance = createConnection(MONGODB_URI);
 
-  if (process.env.NODE_ENV === 'development') {
+  const NODE_ENV = process.env.NODE_ENV;
+  if (!NODE_ENV) throw new Error('NODE_ENV is not set');
+  if (NODE_ENV === 'development') {
     mongoDBGameInstance.useDb('game_server_test');
+    return;
   }
 
   const SELECTED_GAME_SERVER = process.env.SELECTED_GAME_SERVER;
+  if (!SELECTED_GAME_SERVER) throw new Error('SELECTED_GAME_SERVER is not set');
   if (SELECTED_GAME_SERVER === '1') {
     mongoDBGameInstance.useDb('game_server_1');
+    return;
   }
   if (SELECTED_GAME_SERVER === '2') {
     mongoDBGameInstance.useDb('game_server_2');
+    return;
   }
 }
