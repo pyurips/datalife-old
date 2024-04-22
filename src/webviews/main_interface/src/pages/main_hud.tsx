543442e40fdfacb469fe1cd3fcfcb0a26966609b
Pablo Yuri <pabloyuridev@gmail.com>
2024-04-22 15:44:35 -0300

    hotfix: main hud

--- src/webviews/main_interface/src/pages/main_hud.tsx
+++ src/webviews/main_interface/src/pages/main_hud.tsx
@@ -19,24 +19,24 @@ export default function MainHud() {
 
         <div className="flex flex-row items-center gap-[1vw] bg-stone-950/75 p-[0.5vw] rounded-[0.5vw]">
           <div className="flex flex-col gap-[0.5vw]">
-            <Progress className="w-[7vw] h-[0.5vw]" value={100} />
-            <Progress className="w-[7vw] h-[0.5vw]" value={100} />
+            <Progress className="w-[7vw] h-[0.3vw]" value={100} />
+            <Progress className="w-[7vw] h-[0.3vw]" value={100} />
           </div>
           <div className="flex flex-col gap-[0.1vw] items-center">
-            <PiBowlFoodFill className="text-[1.6vw]" />
-            <Progress className="w-[3vw] h-[0.5vw]" value={100} />
+            <PiBowlFoodFill className="text-[1.3vw]" />
+            <Progress className="w-[3vw] h-[0.3vw]" value={100} />
           </div>
           <div className="flex flex-col gap-[0.1vw] items-center">
-            <BiSolidDrink className="text-[1.6vw]" />
-            <Progress className="w-[3vw] h-[0.5vw]" value={100} />
+            <BiSolidDrink className="text-[1.3vw]" />
+            <Progress className="w-[3vw] h-[0.3vw]" value={100} />
           </div>
           <div className="flex flex-col gap-[0.1vw] items-center">
-            <FaToiletPaper className="text-[1.6vw]" />
-            <Progress className="w-[3vw] h-[0.5vw]" value={100} />
+            <FaToiletPaper className="text-[1.3vw]" />
+            <Progress className="w-[3vw] h-[0.3vw]" value={100} />
           </div>
           <div className="flex flex-col gap-[0.1vw] items-center">
-            <IoBedSharp className="text-[1.6vw]" />
-            <Progress className="w-[3vw] h-[0.5vw]" value={35} />
+            <IoBedSharp className="text-[1.3vw]" />
+            <Progress className="w-[3vw] h-[0.3vw]" value={35} />
           </div>
         </div>
       </div>
@@ -47,17 +47,17 @@ export default function MainHud() {
         <div className="flex flex-1 justify-center flex-col gap-[1vw] items-start opacity-70">
           <div className="flex items-center justify-center p-[0.5vw] bg-stone-950 rounded-[0.5vw] gap-[0.5vw] h-[2.5vw] w-[4vw]">
             <IoPersonCircle className="text-[1.5vw]" />
-            <p className="font-bold">B</p>
+            <p>B</p>
           </div>
 
           <div className="flex items-center justify-center p-[0.5vw] bg-stone-950 rounded-[0.5vw] gap-[0.5vw] h-[2.5vw] w-[4vw]">
             <PiCursor className="text-[1.5vw]" />
-            <p className="font-bold">M</p>
+            <p>M</p>
           </div>
 
           <div className="flex items-center justify-center p-[0.5vw] bg-stone-950 rounded-[0.5vw] gap-[0.5vw] h-[2.5vw] w-[4vw]">
             <IoPersonCircle className="text-[1.5vw]" />
-            <p className="font-bold">F2</p>
+            <p>F2</p>
           </div>
         </div>
       )}
