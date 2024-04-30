c840be506ea0b3a412d96d90541a45adadaf593a
Pablo Yuri <pabloyuridev@gmail.com>
2024-04-30 18:33:18 -0300

    hotfix: vehicle hud interaction

--- src/webviews/interactions_interface/src/pages/vehicle.tsx
+++ src/webviews/interactions_interface/src/pages/vehicle.tsx
@@ -3,7 +3,7 @@ import { IoPersonCircle } from 'react-icons/io5';
 export default function Vehicle() {
   return (
     <div className="flex flex-col items-center justify-between w-[200px] h-full">
-      <div className="flex flex-col rounded-xl overflow-hidden bg-stone-900 w-full">
+      <div className="flex flex-col rounded-xl overflow-hidden bg-stone-950 w-full">
         <img
           className="w-full object-cover h-[140px]"
           draggable={false}
@@ -15,7 +15,7 @@ export default function Vehicle() {
         </div>
       </div>
 
-      <div className="flex flex-row items-center gap-2 w-full bg-stone-900 p-2 rounded-xl">
+      <div className="flex flex-row items-center gap-2 bg-stone-950 p-2 rounded-xl">
         <div className='flex items-center justify-center bg-stone-200 size-7 rounded-md'>
           <p className='text-stone-900 font-bold text-xl'>E</p>
         </div>
