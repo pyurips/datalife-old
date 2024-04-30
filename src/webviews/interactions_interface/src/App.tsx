89059d81dd9e91b5ecf59a02fd49c597b6768458
Pablo Yuri <pabloyuridev@gmail.com>
2024-04-30 17:45:12 -0300

    feat: add vehicle interaction alpha hud

--- src/webviews/interactions_interface/src/App.tsx
+++ src/webviews/interactions_interface/src/App.tsx
@@ -1,11 +1,9 @@
-function App() {
+import Vehicle from "./pages/vehicle";
+
+export default function App() {
   return (
     <main className="flex items-center justify-center w-[512px] h-[256px]">
-      <div className="w-[200px] h-full rounded-xl bg-stone-900 p-3">
-        <p>Testando a HUD</p>
-      </div>
+      <Vehicle />
     </main>
   );
 }
-
-export default App;
---
+++ src/webviews/interactions_interface/src/pages/vehicle.tsx
@@ -0,0 +1,26 @@
+import { IoPersonCircle } from 'react-icons/io5';
+
+export default function Vehicle() {
+  return (
+    <div className="flex flex-col items-center justify-between w-[200px] h-full">
+      <div className="flex flex-col rounded-xl overflow-hidden bg-stone-900 w-full">
+        <img
+          className="w-full object-cover h-[140px]"
+          draggable={false}
+          src="https://i.pinimg.com/736x/0d/b5/da/0db5da143c7bf4ace9d3635bd4e35fcc.jpg"
+        />
+        <div className="flex flex-row gap-1 items-center p-2">
+          <IoPersonCircle className="text-xl" />
+          <p className="text-xs">Pabliuri</p>
+        </div>
+      </div>
+
+      <div className="flex flex-row items-center gap-2 w-full bg-stone-900 p-2 rounded-xl">
+        <div className='flex items-center justify-center bg-stone-200 size-7 rounded-md'>
+          <p className='text-stone-900 font-bold text-xl'>E</p>
+        </div>
+        <p className="text-xs">para interagir</p>
+      </div>
+    </div>
+  );
+}
