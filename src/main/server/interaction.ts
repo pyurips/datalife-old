0a113ceeccf2f68a0b117460533b62982604db32
Pablo Yuri <pabloyuridev@gmail.com>
2024-05-11 02:00:55 -0300

    hotfix: some fixes

--- src/main/server/interaction.ts
+++ src/main/server/interaction.ts
@@ -8,22 +8,12 @@ import { DropData } from './types.js';
 import { player_addItemToBelongings } from './player.js';
 import { item_deleteDropById } from './item.js';
 
-export function interaction_check(player: alt.Player) {
-  const closestVehicle = getClosestVehicleFromPlayer(player, 5, true);
-  const closestObject = getClosestDropFromPlayer(player, 3);
-  const closestEntity = [closestVehicle, closestObject].reduce((prev, curr) => {
-    if (!prev) return curr;
-    if (!curr) return prev;
+export function interaction_getDrop(player: alt.Player) {
+  const closestDrop = getClosestDropFromPlayer(player, 3);
 
-    const prevDist = getDistanceBetween(player.pos, prev.pos);
-    const currDist = getDistanceBetween(player.pos, curr.pos);
-
-    return prevDist < currDist ? prev : curr;
-  }, null);
-
-  if (!closestEntity) return;
-  if (closestEntity.hasStreamSyncedMeta('drop')) {
-    const dropData = closestEntity.getStreamSyncedMeta('drop') as DropData;
+  if (!closestDrop) return;
+  if (closestDrop.hasStreamSyncedMeta('drop')) {
+    const dropData = closestDrop.getStreamSyncedMeta('drop') as DropData;
     player_addItemToBelongings(
       player,
       dropData.itemId,
