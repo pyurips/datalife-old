58b6f2c21e1205ab13dce8e77a63408228e0b783
Pablo Yuri <pabloyuridev@gmail.com>
2024-05-24 20:48:08 -0300

    hotfix: altv webview types

--- src/webviews/interactions_interface/src/altv.d.ts
+++ src/webviews/interactions_interface/src/altv.d.ts
@@ -1,11 +1,73 @@
-interface Window {
-  alt?: {
-    on: (
-      event: string,
-      callback: (response: any) => void
-    ) => void;
-    emit: (eventName: string, ...args: any[]) => void;
-    once: (eventName: string, listener: (...args: any[]) => void) => void;
-    off: (eventName: string, listener: (...args: any[]) => void) => void;
-  };
+declare const enum AltPermission {
+  NONE,
+  SCREEN_CAPTURE,
+  WEBRTC,
+  CLIPBOARD_ACCESS,
+  EXTENDED_VOICE_API,
+  All,
 }
+
+interface Alt {
+  /**
+   * Emits specified event across particular client.
+   *
+   * @param eventName Name of the event.
+   * @param args Rest parameters for emit to send.
+   */
+  emit(eventName: string, ...args: any[]): void;
+
+  /**
+   * Unsubscribes from client event handler with specified listener.
+   *
+   * @remarks Listener should be of the same reference as when event was subscribed.
+   *
+   * @param eventName Name of the event.
+   * @param listener Listener that should be removed.
+   */
+  off(eventName: string, listener: (...args: any[]) => void): void;
+
+  /**
+   * Subscribes to client event handler with specified listener.
+   *
+   * @param eventName Name of the event.
+   * @param listener Listener that should be added.
+   */
+  on(eventName: string, listener: (...args: any[]) => void): void;
+
+  /**
+   * Subscribes to client event with specified listener, which only triggers once.
+   *
+   * @param eventName Name of the event.
+   * @param listener Listener that should be added.
+   */
+  once(eventName: string, listener: (...args: any[]) => void): void;
+
+  /**
+   * Gets all the listeners for the specified webview event.
+   *
+   * @param eventName Name of the event.
+   * @returns Array of listener functions for that event.
+   */
+  getEventListeners(eventName: string): ((...args: any[]) => void)[];
+
+  getVersion(): string;
+
+  getBranch(): string;
+
+  getLocale(): string;
+
+  /**
+   * Gets the state of the specified permission.
+   *
+   * @param permId Permission id.
+   * @returns Permission state.
+   */
+  getPermissionState(permId: AltPermission): boolean;
+  getPermissionState<T extends number>(permId: T): boolean;
+}
+
+interface Window {
+  alt: Alt;
+}
+
+declare var alt: Alt;
